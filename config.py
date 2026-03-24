523c529379a6f18cad5ff1a3d6d96eebfe372dad
Vipin Kumar <vipin275869@gmail.com>
2026-03-24 21:41:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/vipin-kumar735/Andaz",
+    "https://github.com/dilchorofficial/Bts-army-music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
